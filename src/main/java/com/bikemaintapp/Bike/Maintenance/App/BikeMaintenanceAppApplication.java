858d1329d20ac9665f6e82a50a583f090405c80b
Michael Alarcon <malarconii@yahoo.com>
2017-09-28 22:00:51 -0700

    added Gradle build/SpringBoot dependencies

---
+++ src/main/java/com/bikemaintapp/Bike/Maintenance/App/BikeMaintenanceAppApplication.java
@@ -0,0 +1,12 @@
+package com.bikemaintapp.Bike.Maintenance.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeMaintenanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeMaintenanceAppApplication.class, args);
+	}
+}
