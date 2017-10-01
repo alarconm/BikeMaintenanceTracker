fa8e21f5767c1cd98a66636585782ee4f6afa5d0
Michael Alarcon <malarconii@yahoo.com>
2017-09-30 20:50:59 -0700

    Updated gitingore to ignore .idea files

--- src/main/java/com/bikemaintapp/Bike/Maintenance/App/controllers/UserController.java
+++ src/main/java/com/bikemaintapp/Bike/Maintenance/App/controllers/UserController.java
@@ -8,8 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;i
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
 
