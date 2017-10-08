8d38b1ec314331e7e896f8be7d3f13bd7be97cf5
Avery VanKirk <averyvankirk@gmail.com>
2017-10-08 12:28:54 -0700

    ride add page loads only bikes matching users ID

--- src/main/java/com/bikemaintapp/Bike/Maintenance/App/models/data/RideDao.java
+++ src/main/java/com/bikemaintapp/Bike/Maintenance/App/models/data/RideDao.java
@@ -5,8 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface RideDao extends CrudRepository<Ride, Integer>{
+
+
+    List<Ride> findRideByBikeID(int bikeID);
 }
+
