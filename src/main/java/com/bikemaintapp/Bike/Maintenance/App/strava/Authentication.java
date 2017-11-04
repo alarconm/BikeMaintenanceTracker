68e2bc1b8f5bb0ada6c102e97f75f1374c60e79f
Michael Alarcon <malarconii@yahoo.com>
2017-11-04 16:53:28 -0700

    reconfigured strava to work on heroku

--- src/main/java/com/bikemaintapp/Bike/Maintenance/App/strava/Authentication.java
+++ src/main/java/com/bikemaintapp/Bike/Maintenance/App/strava/Authentication.java
@@ -6,7 +6,7 @@ public class Authentication {
 
     RestTemplate restTemplate = new RestTemplate();
     StravaRide ride = restTemplate.getForObject("https://www.strava.com/oauth/authorize?" +
-            "client_id=21098&response_type=code&redirect_uri=http://127.0.0.1", StravaRide.class);
+            "client_id=21098&response_type=code&redirect_uri=bike-maint.herokuapp.com", StravaRide.class);
 
 
 }
